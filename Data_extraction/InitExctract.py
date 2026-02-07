18ae62955673e15b644f9ff8054ecd1400a4c6c1
John <johnisaacarthur@gmail.com>
2026-02-06 17:50:47 -0700

    I believe I may attempt to code and complete this project without ever using the actual data set and hope my code is foolproof. My laptop almost cannot handle the simple queries I am presenting it with already, I believe I will experience a complete system failure if I attempt the much larger queries I plan on attempting later on in this project.

--- Data_extraction/InitExctract.py
+++ Data_extraction/InitExctract.py
@@ -4,11 +4,19 @@ bs = '~/code/nba_data_explored/Data_extraction/PlayerStatistics.parquet'
 
 df = pd.read_parquet(bs)
 
-giannisGames = df.query('(firstName == Giannis)')
+giannisGames = df.query('(firstName == "Giannis")')
 
 # I'm not sure if .query() works with strings or just with numbers, this may be my problem.
+# .query() does in fact work with strings, but you need to use an extra set of quotes
 
 print(giannisGames.head(10))
 
 # print(df.info())
 
+# We have a couple problems here. Firstly, games where a player does not play, it apppears they still have
+# a recorded stat block. Because of the empty rebounds, steals, points, assists, etc. this will cause
+# incorrect averages and skew the data. I need to find a way to avoid these missed games in the final totals. 
+# I also currently have very good reason to believe that my computer will struggle heavily with this data set.
+# Using the notebook files to query the data set resulted in VS code crashing twice, and simple queries like
+# the one I just did finding the stats for Giannis's last 10 games took around 20 seconds to execute. I may
+# not be able to complete this project as I do not have access to better machinery.
