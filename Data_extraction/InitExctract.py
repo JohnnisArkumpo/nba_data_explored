a42f8123b93c203c6f4be2f95e55bf3a26de22d1
John <johnisaacarthur@gmail.com>
2026-02-06 17:38:35 -0700

    Plan prepared and querying started

--- Data_extraction/InitExctract.py
+++ Data_extraction/InitExctract.py
@@ -4,5 +4,11 @@ bs = '~/code/nba_data_explored/Data_extraction/PlayerStatistics.parquet'
 
 df = pd.read_parquet(bs)
 
-print(df.info())
+giannisGames = df.query('(firstName == Giannis)')
+
+# I'm not sure if .query() works with strings or just with numbers, this may be my problem.
+
+print(giannisGames.head(10))
+
+# print(df.info())
 
