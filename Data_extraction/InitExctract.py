6e2bf6c57f8996451dab24fa20fab5b99647c13e
John <johnisaacarthur@gmail.com>
2026-02-07 17:45:43 -0700

    gonna save rq and restart, maybe that'll save me. 4 gigs of ram ain't enough.

--- Data_extraction/InitExctract.py
+++ Data_extraction/InitExctract.py
@@ -6,6 +6,8 @@ df = pd.read_parquet(bs)
 
 gp = df.query('numMinutes > 0')
 
+# print(gp.head(5))
+
 # gotta create a new file
 
 path_to_np = '~/code/nba_data_explored/Data_extraction/PlayerStatsClean.parquet'
