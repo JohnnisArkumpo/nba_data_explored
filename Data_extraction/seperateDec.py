d7a2a64532655bc4707cd3657b14f2f3d9736197
John <johnisaacarthur@gmail.com>
2026-02-07 19:20:41 -0700

    to_datetime is givin me problems. Imma hop off to let the computer charge for a minute and get back to this later.

--- Data_extraction/seperateDec.py
+++ Data_extraction/seperateDec.py
@@ -8,3 +8,14 @@ df = pd.read_parquet(clns)
 
 # print(df["gameDate"].tail(50)) Data set starts 1951-11-11
 
+dcpath = '~/code/nba_data_explored/Data_extraction/nbaStats50s.parquet'
+
+# gotta filter by date. I'm not sure how to do that as the date is in strings.
+
+df['gameDate'] = pd.to_datetime(df['gameDate'])
+
+decade = df.query('gameDate < 1960-01-01')
+
+decade.to_parquet(dcpath, index=False)
+
+# gameDate is before 1960-01-01 for the first query. Between required dates for each next one.
